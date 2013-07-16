9ef7f4b6925f712916071cbd9b6ce7ced82cdf65
Gildas Quéméner <gildas.quemener@gmail.com>
2013-07-16 10:04:05 +0200

    Renamed method into step

--- src/Behat/AnalysisExtension/Formatter/AnalysisFormatter.php
+++ src/Behat/AnalysisExtension/Formatter/AnalysisFormatter.php
@@ -4,7 +4,7 @@ namespace Behat\AnalysisExtension\Formatter;
 
 use Behat\Behat\Formatter\ProgressFormatter;
 use Behat\Behat\Event\SuiteEvent;
-use Behat\Behat\DataCollector\LoggerDataCollector;
+use Behat\AnalysisExtension\DataCollector\AnalysisDataCollector;
 
 class AnalysisFormatter extends ProgressFormatter
 {
@@ -15,33 +15,33 @@ class AnalysisFormatter extends ProgressFormatter
         $logger = $event->getLogger();
         $this->writeln("\n{+passed}Behat Steps Analysis{-passed}");
         $this->writeln('====================');
-        $this->printMethodCalls($logger);
-        $this->printMergeableMethods($logger);
+        $this->printStepsUsages($logger);
+        $this->printMergeableSteps($logger);
     }
 
-    private function printMethodCalls(LoggerDataCollector $logger)
+    private function printStepsUsages(AnalysisDataCollector $logger)
     {
-        $methodCalls = $logger->getMethodCalls();
-        arsort($methodCalls);
+        $stepsUsages = $logger->getStepsUsages();
+        arsort($stepsUsages);
 
-        $onceCalled = array_filter($methodCalls, function ($count) {
+        $onceCalled = array_filter($stepsUsages, function ($count) {
             return 1 === $count;
         });
         $this->writeln(sprintf('{+passed}%d{-passed} steps were used once.', count($onceCalled)));
-        reset($methodCalls);
+        reset($stepsUsages);
         $this->writeln(sprintf(
             'The most used step is {+passed}%s{-passed} with {+passed}%d{-passed} calls.',
-            key($methodCalls), current($methodCalls)
+            key($stepsUsages), current($stepsUsages)
         ));
     }
 
-    private function printMergeableMethods(LoggerDataCollector $logger)
+    private function printMergeableSteps(AnalysisDataCollector $logger)
     {
         $this->writeln('Some steps might be merged to reduce their implementation redundancy:');
-        foreach ($logger->getMergeableMethods() as $method) {
+        foreach ($logger->getMergeableSteps() as $step) {
             $this->writeln(sprintf(
                 '  - {+undefined}%s{-undefined} and {+undefined}%s{-undefined}',
-                $method['source'], $method['compare']
+                $step['source'], $step['compare']
             ));
         }
     }
